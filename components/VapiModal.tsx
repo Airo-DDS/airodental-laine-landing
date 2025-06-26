e125aedb372d43d1725727fa35b7737d153bcb68
justDenzil <vikash.denzil@gmail.com>
2025-06-26 23:45:56 +0530

    AD numbers

--- components/VapiModal.tsx
+++ components/VapiModal.tsx
@@ -73,7 +73,7 @@ export default function VapiModal({ isOpen, onClose }: VapiModalProps) {
                   href="tel:+19016104179"
                   className="inline-flex items-center justify-center px-4 py-2 bg-gray-50 hover:bg-gray-100 border border-gray-200 rounded-lg transition-colors text-gray-900 font-medium text-lg"
                 >
-                  +1 (901) 610 4179
+                  +1 (910) 418 1836
                 </a>
               </div>
             </div>
